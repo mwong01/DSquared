3f1828bdb2ced6d825952fdafce34ad525c9ff1b
Fatima Altaf <fatima.altaf@gmail.com>
2019-11-16 22:08:34 +0000

    add files that are working on the homepage and routes

--- server.js
+++ server.js
@@ -33,13 +33,13 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
+const pollsRoutes = require("./routes/polls");
 const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/polls", pollsRoutes(db));
+app.use("/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
@@ -48,6 +48,10 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/", (req, res) => {
+  res.redirect("/polls")
 });
 
 app.listen(PORT, () => {
