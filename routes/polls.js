4877a8adc9ab5e7e99db59f4d9c266fe412055f5
Fatima Altaf <fatima.altaf@gmail.com>
2019-11-18 21:16:05 +0000

    add /poll and functions to link to polls

--- routes/polls.js
+++ routes/polls.js
@@ -7,21 +7,51 @@
 
 const express = require('express');
 const router  = express.Router();
+const database = require("./database");
 
-module.exports = (db) => {
-  router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`;
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-  return router;
-};
+// module.exports = (db) => {
+//   router.get("/", (req, res) => {
+//     let query = `SELECT * FROM widgets`;
+//     console.log(query);
+//     db.query(query)
+//       .then(data => {
+//         const widgets = data.rows;
+//         res.json({ widgets });
+//       })
+//       .catch(err => {
+//         res
+//           .status(500)
+//           .json({ error: err.message });
+//       });
+//   });
+//   return router;
+// };
+
+module.exports = function(database) {
+
+  // Create a new poll
+  
+    router.post("/", (req, res) => {
+      if (req.body.title === "" || req.body.email === "") {
+        res.status(400);
+        res.send("400 error - Bad Request: No title or email entered. Please try again");   
+      }
+    });
+    //   else {  
+    //     const pollId = req.params.id
+    //     database.addPoll(pollId)
+    //     .then( (id) => {
+    //       // res.redirect("/polls/:id/links"))
+    //       res.send('i hope it worked')
+      
+    //     }).catch(e => res.send(e));
+    //   }
+    // })
+  // Page that renders admin link and shortened url for participants
+  
+    router.get("/polls/:id/links", (req, res) => {
+      res.render("polls_links");
+    });
+  
+    return router;
+  };
