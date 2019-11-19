75251d32948fdebfa4c8400f6e850a0d9d291d1b
mwong01 <mattw90@gmail.com>
2019-11-19 22:14:15 +0000

    sql

---
+++ db/schema/01_polls.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS polls CASCADE;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE polls (
+  id uuid PRIMARY KEY default uuid_generate_v4(),
+  public_id uuid NOT NULL default uuid_generate_v4(),
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  email VARCHAR(255) NOT NULL
+);
+CREATE INDEX IF NOT EXISTS polls_public_id_index ON polls ("public_id");
