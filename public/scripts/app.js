a30b95aa9ce1614ca8007fc1fd44370f62475bfb
Fatima Altaf <fatima.altaf@gmail.com>
2019-11-19 18:10:13 +0000

    fix homepage poll form and database submission migration

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,29 @@
+ // Poll function for dynamic poll form
+ const createLiChoices = function(number){
+  let $choices = $('<article  id="choice-container">').addClass('list');
+  let markup = ``;
+  for (let i = 1; i <= number; i++) {
+    let singleChoice = `
+      <textarea name="choiceSub[${i}]" class="form-control" rows="1" style="width: 200px;" placeholder="Choose an option!"></textarea>
+    `;
+    markup += singleChoice;
+  }
+  return $choices.append(markup);
+};
 
+// AJAX REQUESTS
+$(document).ready(function(){
+  $("#first-button").click( () => {
+    $("#hide-button").slideToggle();
 
-// // Poll function for dynamic poll form
-// const createLiChoices = function(number){
-//   let $choices = $('<article  id="choice-container">').addClass('list');
-//   let markup = ``;
-//   for (let i = 1; i <= number; i++) {
-//     let singleChoice = `
-//       <textarea  name="choiceSub" class="form-control" id="${i}" rows="1" style="width: 200px;" placeholder="Choose an option!"></textarea>
-//     `;
-//     markup += singleChoice;
-//   }
-//   return $choices.append(markup);
-// };
+    event.preventDefault();
+    const choice = $("#options").serialize();
+    const number = choice.slice(choice.length-1, choice.length);
+    const numInt = parseInt(number);
 
-// // AJAX REQUESTS
-// $(document).ready(function(){
-//   $("#first-button").click( () => {
-//     $("#hide-button").slideToggle();
-
-//     event.preventDefault();
-//     const choice = $("#options").serialize();
-//     const number = choice.slice(choice.length-1, choice.length);
-//     const numInt = parseInt(number);
-
-//     $('#option-creator').append(createLiChoices(numInt));
-//     setTimeout(function(){ 
-//       $("#box-two").slideToggle();
-//     }, 1000);
-//   });
-// });
+    $('#option-creator').append(createLiChoices(numInt));
+    setTimeout(function(){ 
+      $("#box-two").slideToggle();
+    }, 1000);
+  });
+});
